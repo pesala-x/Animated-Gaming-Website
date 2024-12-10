9fe608652d7ddf6f9b0f22eb66703afd776bebcb
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-11 00:21:03 +0530

    setup web application successfully

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <div>App</div>
+        <h1 className="text-3xl text-orange-500 font-bold"> Welcome to this Gaming Website</h1>
     )
 }
 export default App
