7336eef39c374e148ef96b391c5c6e4346f41c67
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-11 11:10:11 +0530

    start to build Hero section

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,10 @@
-import React from 'react'
+import Hero from "./components/Hero.jsx";
 
 const App = () => {
     return (
-        <h1 className="text-3xl text-violet-300 font-bold"> Welcome to this Gaming Website</h1>
+        <main>
+            <Hero />
+        </main>
     )
 }
 export default App
