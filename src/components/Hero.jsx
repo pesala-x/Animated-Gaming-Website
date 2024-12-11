7b963efa91cce77a996f85847dab7dbcac6c83e9
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-11 19:10:45 +0530

    attach player and hero section background video loaded & animated

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -25,11 +25,13 @@ const Hero = () => {
         <div className= "relative h-dvh w-screen overflow-x-hidden">
             <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
                 <div>
-                    <div className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
-                        <div onClick={handleMiniVdClick} className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
+                    <div
+                        className="mask-clip-path absolute-center absolute z-50 size-64 cursor-pointer overflow-hidden rounded-lg">
+                        <div onClick={handleMiniVdClick}
+                             className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                             <video
                                 ref={nextVideoRef}
-                                src={getVideoSrc(currentIndex +1)}
+                                src={getVideoSrc(upcomingVideoIndex)}
                                 loop
                                 muted
                                 id="current-video"
@@ -38,7 +40,32 @@ const Hero = () => {
                             />
                         </div>
                     </div>
+
+                    <video
+                        ref={nextVideoRef}
+                        src={getVideoSrc(currentIndex)}
+                        loop
+                        muted
+                        id="next-video"
+                        className="absolute-center invisible absolute z-20 size-64 object-cover object-center"
+                        onLoadedData={handleVideoLoad}
+                    />
+
+                    <video
+                        src={getVideoSrc(
+                            currentIndex === totalVideos - 1 ? 1 : currentIndex
+                        )}
+                        autoPlay
+                        loop
+                        muted
+                        className="absolute left-0 top-0 size-full object-cover object-center"
+                        onLoadedData={handleVideoLoad}
+                    />
                 </div>
+
+                <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
+                    G<b>A</b>MING
+                </h1>
             </div>
         </div>
     )
