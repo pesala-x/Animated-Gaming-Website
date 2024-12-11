af55a95b3014890b33db071beedd9e47f5762f82
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-11 18:20:03 +0530

    hero section main structures implemented

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import Hero from "./components/Hero.jsx";
 
 const App = () => {
     return (
-        <main>
+        <main className="relative min-h-screen w-screen overflow-x-hidden ">
             <Hero />
         </main>
     )
