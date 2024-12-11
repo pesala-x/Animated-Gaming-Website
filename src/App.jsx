3a496d9ee13ce2aa5f4733ed328842ae99d00338
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-11 11:05:58 +0530

    setup color themes and configure with tailwind.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const App = () => {
     return (
-        <h1 className="text-3xl text-orange-500 font-bold"> Welcome to this Gaming Website</h1>
+        <h1 className="text-3xl text-violet-300 font-bold"> Welcome to this Gaming Website</h1>
     )
 }
 export default App
