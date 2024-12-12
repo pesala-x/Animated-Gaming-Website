361b0629b2103fb957491b36b090a829bfe73bf2
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-12 23:05:02 +0530

    Hero section video framer moving animation implemented with GSAP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,6 +1,8 @@
 import {useRef, useState} from "react";
 import Button from "./Button.jsx";
 import {TiLocationArrow} from "react-icons/ti";
+import {useGSAP} from "@gsap/react";
+import gsap from "gsap";
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -22,6 +24,28 @@ const Hero = () => {
         setCurrentIndex(upcomingVideoIndex);
     };
 
+    useGSAP(()=>{
+        if(hasClicked){
+            gsap.set('#next-video', {visibility:'visible'});
+
+            gsap.to("#next-video", {
+                transformOrigin: "center center",
+                scale: 1,
+                width: "100%",
+                height: "100%",
+                duration: 1,
+                ease: "power1.inOut",
+                onStart: () => nextVideoRef.current.play(),
+            });
+
+            gsap.from("#current-video", {
+                transformOrigin: "center center",
+                scale: 0,
+                duration: 1.5,
+                ease: "power1.inOut",
+            });
+        }
+    },{dependencies:[currentIndex],revertOnUpdate:true})
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
     return (
         <div className="relative h-dvh w-screen overflow-x-hidden">
@@ -33,7 +57,7 @@ const Hero = () => {
                              className="origin-center scale-50 opacity-0 transition-all duration-500 ease-in hover:scale-100 hover:opacity-100">
                             <video
                                 ref={nextVideoRef}
-                                src={getVideoSrc(upcomingVideoIndex)}
+                                src={getVideoSrc((currentIndex % totalVideos) + 1)}
                                 loop
                                 muted
                                 id="current-video"
