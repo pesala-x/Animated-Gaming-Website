f78a279f9ec515472aa113558087dbf70b1e7de3
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-12 21:25:37 +0530

    hero section topBar components are implemented

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,4 +1,6 @@
 import {useRef, useState} from "react";
+import Button from "./Button.jsx";
+import {TiLocationArrow} from "react-icons/ti";
 
 const Hero = () => {
     const [currentIndex, setCurrentIndex] = useState(1);
@@ -22,7 +24,7 @@ const Hero = () => {
 
     const getVideoSrc = (index) => `videos/hero-${index}.mp4`;
     return (
-        <div className= "relative h-dvh w-screen overflow-x-hidden">
+        <div className="relative h-dvh w-screen overflow-x-hidden">
             <div id="video-frame" className="relative z-10 h-dvh w-screen overflow-hidden rounded-lg bg-blue-75">
                 <div>
                     <div
@@ -66,7 +68,30 @@ const Hero = () => {
                 <h1 className="special-font hero-heading absolute bottom-5 right-5 z-40 text-blue-75">
                     G<b>A</b>MING
                 </h1>
+
+                <div className='absolute left-0 top-0 z-40 size-full'>
+                    <div className='mt-24 px-5 sm:px-10 '>
+                        <h1 className="special-font hero-heading text-blue-100">
+                            redefi<b>n</b>e
+                        </h1>
+
+                        <p className="mb-5 max-w-64 font-robert-regular text-blue-100">
+                            Enter the Metagame Layer <br/> Unleash the Play Economy
+                        </p>
+
+                        <Button
+                            id="watch-trailer"
+                            title="Watch trailer"
+                            leftIcon={<TiLocationArrow/>}
+                            containerClass="!bg-yellow-300 flex-center gap-1"
+                        />
+                    </div>
+                </div>
             </div>
+
+            <h1 className="special-font hero-heading absolute bottom-5 right-5 text-black">
+                G<b>A</b>MING
+            </h1>
         </div>
     )
 }
