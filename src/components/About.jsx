db980e2e39458284841a55a455fe356d566287bd
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-23 11:06:36 +0530

    start to implement about section

---
+++ src/components/About.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div id='about' className='min-h-screen w-screen '>
+            <div className='relative mb-8 mt-36 flex flex-col items-center gap-5'>
+                <h2 className='font-general text-sm uppercase md:text-[10px]'>Welcome to League of Legends</h2>
+
+                <div className='mt-5 text-center text-4xl uppercase leading-[0.8] md:text-[6rem]'>
+                    Disc<b>o</b>ver the world's <br/> l<b>a</b>rgest shared adventure
+                </div>
+
+                <div>
+
+                </div>
+            </div>
+        </div>
+    )
+}
+export default About
