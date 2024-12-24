b9526202fa5df70970698ee0bd25b2382e4b616f
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-24 10:47:25 +0530

    start to implement navigation Bar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
+import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
     return (
         <main className="relative min-h-screen w-screen overflow-x-hidden ">
+            <NavBar />
             <Hero />
             <About />
         </main>
