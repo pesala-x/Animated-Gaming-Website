1782bc4554e6c88be7924a482664158d57b27cdb
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-25 00:09:43 +0530

    Navigation-bar right side implemented with awesome audio play button

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,10 +1,29 @@
-import {useRef} from "react";
+import {useEffect, useRef, useState} from "react";
 import Button from "./Button.jsx";
 import {TiLocationArrow} from "react-icons/ti";
 
-const NavBar = () => {
-    const navContainerRef = useRef(null);
+// Nav right array values
+const navItems = ['Nexus', 'Vault', 'Prologue', 'About', 'Contact' ];
 
+const NavBar = () => {
+    const [isAudioPlaying, setIsAudioPlaying] = useState(false)
+    const [isIndicatorActive, setIsIndicatorActive] = useState(false);
+
+    const navContainerRef = useRef(null);
+    const audioElementRef = useRef(null);
+
+    const toggleAudioIndicator = () => {
+        setIsAudioPlaying((prev) => !prev);
+        setIsIndicatorActive((prev) => !prev);
+    }
+
+    useEffect(() => {
+        if(isAudioPlaying) {
+            audioElementRef.current.play();
+        }else {
+            audioElementRef.current.pause();
+        }
+    },[isAudioPlaying])
     return (
         <div ref={navContainerRef} className='fixed inset-x-0 top-4 z-50 h-16 border-none transition-all duration-700 sm:inset-x-6'>
             <header className='absolute top-1/2 w-full -translate-y-1/2'>
@@ -22,6 +41,44 @@ const NavBar = () => {
                             rightIcon={<TiLocationArrow/>}
                             containerClass='bg-blue-50 md:flex hidden items-center justify-center gap-1'
                         />
+                    </div>
+
+                {/* Right Side of the navBar with audio Button */}
+                    <div className='flex h-full items-center'>
+                        {/*this div for smaller devices*/}
+                        <div className='hidden md:block'>
+                            {/* nav items with declared array*/}
+                            {navItems.map((item, index) => (
+                                <a
+                                    key={index}
+                                    href={`#${item.toLowerCase()}`}
+                                    className="nav-hover-btn"
+                                >
+                                    {item}
+                                </a>
+                            ))}
+                        </div>
+                        <button
+                            onClick={toggleAudioIndicator}
+                            className="ml-10 flex items-center space-x-0.5"
+                        >
+                            <audio
+                                ref={audioElementRef}
+                                className="hidden"
+                                src="/audio/loop.mp3"
+                                loop
+                            />
+                            {/*audio indicator bar declare with array 6 bars*/}
+                            {[1, 2, 3, 4].map((bar) => (
+                                <div
+                                    key={bar}
+                                    className={`indicator-line ${isIndicatorActive ? 'active' : ''}`}
+                                    style={{
+                                        animationDelay: `${bar * 0.1}s`,
+                                    }}
+                                />
+                            ))}
+                        </button>
                     </div>
                 </nav>
             </header>
