25ef60ef28f8c2031482592bed98e2658d59a1ae
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-24 21:06:03 +0530

    Navigation-bar left side implemented

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import NavBar from "./components/NavBar.jsx";
 
 const App = () => {
     return (
-        <main className="relative min-h-screen w-screen overflow-x-hidden ">
+        <main className="relative min-h-screen w-screen overflow-x-hidden bg-zinc-600">
             <NavBar />
             <Hero />
             <About />
