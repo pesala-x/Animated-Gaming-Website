aea0931b1bb1a9512f5a8deb33620c1d03421c64
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-26 22:45:35 +0530

    start to build(implement) Features section

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import Hero from "./components/Hero.jsx";
 import About from "./components/About.jsx";
 import NavBar from "./components/NavBar.jsx";
+import Features from "./components/Features.jsx";
 
 const App = () => {
     return (
@@ -8,6 +9,7 @@ const App = () => {
             <NavBar />
             <Hero />
             <About />
+            <Features />
         </main>
     )
 }
