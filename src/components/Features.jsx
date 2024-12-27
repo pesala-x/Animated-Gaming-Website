0160dc8e24027334a68fc72c77e08e07aed3cea6
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-27 22:54:36 +0530

    all feature cards implemented and make responsive them

--- src/components/Features.jsx
+++ src/components/Features.jsx
@@ -1,5 +1,9 @@
+// eslint-disable-next-line no-unused-vars
 import React from 'react'
+import {TiLocationArrow} from "react-icons/ti";
+
 //  reusable BentoCards
+// eslint-disable-next-line react/prop-types
 const BentoCard = ({src,title,description}) => {
   return(
       <div className='relative size-full '>
@@ -28,22 +32,81 @@ const Features = () => {
     return (
         <section className='bg-black pb-52'>
             <div className="container mx-auto px-3 md:px-10">
-                <div className='px-5 py-32 '>
+                <div className='px-5 py-32'>
                     <p className="font-circular-web text-lg text-blue-50">Into the Metagame Layer</p>
                     <p className="max-w-md font-circular-web text-lg text-blue-50 opacity-50">
                         Immerse yourself in a rich and ever-expanding universe where a vibrant
                         array of products converge into an interconnected overlay experience
                         on your world.
                     </p>
-                    <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+                </div>
+                <div className='border-hsla relative mb-7 h-96 w-full overflow-hidden rounded-md md:h-[65vh]'>
+                    <BentoCard
+                        src="videos/feature-1.mp4"
+                        title={
+                            <>
+                                radia<b>n</b>t
+                            </>
+                        }
+                        description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+                    />
+                </div>
+                <div className='grid h-[135vh] grid-cols-2 grid-rows-3 gap-7 '>
+                    {/*second Bento card */}
+                    <div className='bento-tilt_1 row-span-1 md:col-span-1 md:row-span-2 '>
                         <BentoCard
-                            src="videos/feature-1.mp4"
+                            src="videos/feature-2.mp4"
                             title={
                                 <>
-                                    radia<b>n</b>t
+                                    zig<b>m</b>a
                                 </>
                             }
-                            description="A cross-platform metagame app, turning your activities across Web2 and Web3 games into a rewarding adventure."
+                            description="An anime and gaming-inspired NFT collection - the IP primed for expansion."
+                            isComingSoon
+                        />
+                    </div>
+                    {/*third bento card*/}
+                    <div className="bento-tilt_1 row-span-1 ms-32 md:col-span-1 md:ms-0">
+                        <BentoCard
+                            src="videos/feature-3.mp4"
+                            title={
+                                <>
+                                    n<b>e</b>xus
+                                </>
+                            }
+                            description="A gamified social hub, adding a new dimension of play to social interaction for Web3 communities."
+                            isComingSoon
+                        />
+                    </div>
+                    {/*Fourth bento card*/}
+                    <div className="bento-tilt_1 me-14 md:col-span-1 md:me-0">
+                        <BentoCard
+                            src="videos/feature-4.mp4"
+                            title={
+                                <>
+                                    az<b>u</b>l
+                                </>
+                            }
+                            description="A cross-world AI Agent - elevating your gameplay to be more fun and productive."
+                            isComingSoon
+                        />
+                    </div>
+
+                    {/*Final Cards*/}
+                    <div className='bento-tilt_2'>
+                        <div className='flex size-full flex-col justify-between bg-violet-300 p-5'>
+                            <h1 className='bento-title special-font max-w-64 text-black '>M<b>o</b>re Co<b>m</b>ing s<b>o</b><b>o</b>n!</h1>
+                            <TiLocationArrow className='m-5 scale-[5] self-end '/>
+                        </div>
+                    </div>
+                    {/*Additional Card*/}
+                    <div className="bento-tilt_2">
+                        <video
+                            src='videos/hero-5.mp4'
+                            loop
+                            muted
+                            autoPlay
+                            className='size-full object-cover object-center'
                         />
                     </div>
                 </div>
