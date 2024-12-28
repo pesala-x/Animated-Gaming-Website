b497cf584c83d7e142dd6219862d9f6dd8f7b12d
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-28 17:48:11 +0530

    footer section all elements implemented

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -14,7 +14,7 @@ const Footer = () => {
         <footer className="w-screen bg-violet-300 py-4 text-black ">
             <div className='container mx-auto flex flex-col items-center justify-between gap-4 px-4 md:flex-row'>
                 <p className="text-center text-sm font-light md:text-left">
-                    &copy;Nova 2024. All rights reserved
+                    &copy;Pesala-X 2024. All rights reserved
                 </p>
 
                 <div className="flex justify-center gap-4  md:justify-start">
@@ -30,7 +30,12 @@ const Footer = () => {
                         </a>
                     ))}
                 </div>
-
+                <a
+                    href="#privacy-policy"
+                    className="text-center text-sm font-light hover:underline md:text-right"
+                >
+                    Privacy Policy
+                </a>
             </div>
         </footer>
     )
