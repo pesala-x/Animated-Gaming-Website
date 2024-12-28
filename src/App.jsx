4c3b6586946846d2ef23795adbb73c5b20759c81
Pesala winodith <gkpesalawinodith@gmail.com>
2024-12-28 10:22:44 +0530

    start to implement Contact section with Footer

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import NavBar from "./components/NavBar.jsx";
 import Features from "./components/Features.jsx";
 import Story from "./components/Story.jsx";
 import Contact from "./components/Contact.jsx";
+import Footer from "./components/Footer.jsx";
 
 const App = () => {
     return (
@@ -14,6 +15,7 @@ const App = () => {
             <Features />
             <Story />
             <Contact />
+            <Footer />
         </main>
     )
 }
